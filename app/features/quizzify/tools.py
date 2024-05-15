8a2ebda2b5ae792119a89abc95156a1c31b08d97
abeel2019029 <u2019029@giki.edu.pk>
2024-05-16 03:54:05 +0500

    features decoupling from GCS

--- app/features/quizzify/tools.py
+++ app/features/quizzify/tools.py
@@ -8,17 +8,22 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_google_vertexai import VertexAIEmbeddings, VertexAI
 from services.schemas import GCS_File
-from services.gcp import (
-    setup_logger, 
-    read_blob_to_string,
-    download_from_gcs
-    )
+# from services.gcp import (
+#     setup_logger, 
+#     read_blob_to_string,
+#     download_from_gcs
+#     )
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
-
+import json
 logger = setup_logger()
+
+def read_text_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
 
 class RAGRunnable:
     def __init__(self, func):
@@ -33,6 +38,25 @@ class RAGRunnable:
     def __call__(self, *args, **kwargs):
         return self.func(*args, **kwargs)
 
+# class UploadPDFLoader:
+#     def __init__(self, files: List[UploadFile]):
+#         self.files = files
+
+#     def load(self) -> List[Document]:
+#         documents = []
+
+#         for upload_file in self.files:
+#             with upload_file.file as pdf_file:  
+#                 pdf_reader = PdfReader(pdf_file)
+
+#                 for i, page in enumerate(pdf_reader.pages):
+#                     page_content = page.extract_text()
+#                     metadata = {"source": upload_file.filename, "page_number": i + 1}
+
+#                     doc = Document(page_content=page_content, metadata=metadata)
+#                     documents.append(doc)
+
+#         return documents
 class UploadPDFLoader:
     def __init__(self, files: List[UploadFile]):
         self.files = files
@@ -41,7 +65,7 @@ class UploadPDFLoader:
         documents = []
 
         for upload_file in self.files:
-            with upload_file.file as pdf_file:  
+            with upload_file.file as pdf_file:
                 pdf_reader = PdfReader(pdf_file)
 
                 for i, page in enumerate(pdf_reader.pages):
@@ -52,6 +76,7 @@ class UploadPDFLoader:
                     documents.append(doc)
 
         return documents
+
 
 class BytesFileLoader:
     def __init__(self, files: List[Tuple[BytesIO, str]]):
@@ -76,71 +101,143 @@ class BytesFileLoader:
             
         return documents
 
-class GCSLoader:
-    def __init__(
-        self,
-        bucket_name = "kai-ai-f63c8.appspot.com",
-        file_loader = BytesFileLoader,
-        expected_file_type = "pdf"
-    ):
-        self.loader = file_loader
-        self.bucket_name = bucket_name
+# class GCSLoader:
+#     def __init__(
+#         self,
+#         bucket_name = "kai-ai-f63c8.appspot.com",
+#         file_loader = BytesFileLoader,
+#         expected_file_type = "pdf"
+#     ):
+#         self.loader = file_loader
+#         self.bucket_name = bucket_name
         
-        if expected_file_type in ['pdf']:
-            self.expected_file_type = expected_file_type
-        else:
-            raise ValueError(f"Unsupported file type: {expected_file_type}")
+#         if expected_file_type in ['pdf']:
+#             self.expected_file_type = expected_file_type
+#         else:
+#             raise ValueError(f"Unsupported file type: {expected_file_type}")
     
-    def load(self, file_objects: list[GCS_File]):
-        loaded_files = []
+#     def load(self, file_objects: list[GCS_File]):
+#         loaded_files = []
         
-        for file_object in file_objects:
-            file_path = file_object.filePath
-            file_type = file_object.filename.split(".")[-1]
+#         for file_object in file_objects:
+#             file_path = file_object.filePath
+#             file_type = file_object.filename.split(".")[-1]
             
+#             if file_type != self.expected_file_type:
+#                 raise ValueError(f"Expected file type: {self.expected_file_type}, but got: {file_type}")
+            
+#             bytes_io_obj = download_from_gcs(self.bucket_name, file_path)
+#             loaded_files.append((bytes_io_obj, file_type))
+#             logger.info(f"Successfully loaded file: {file_object.filename}")
+
+#         file_loader = self.loader(loaded_files)
+#         file_documents = file_loader.load()
+
+#         return file_documents
+class LocalFileLoader:
+    def __init__(self, file_paths: List[str], expected_file_type="pdf"):
+        self.file_paths = file_paths
+        self.expected_file_type = expected_file_type
+
+    def load(self) -> List[Document]:
+        documents = []
+
+        for file_path in self.file_paths:
+            file_type = file_path.split(".")[-1]
+
             if file_type != self.expected_file_type:
                 raise ValueError(f"Expected file type: {self.expected_file_type}, but got: {file_type}")
+
+            with open(file_path, 'rb') as file:
+                pdf_reader = PdfReader(file)
+
+                for i, page in enumerate(pdf_reader.pages):
+                    page_content = page.extract_text()
+                    metadata = {"source": file_path, "page_number": i + 1}
+
+                    doc = Document(page_content=page_content, metadata=metadata)
+                    documents.append(doc)
+
+        return documents
+
+
+# class RAGpipeline:
+#     def __init__(self, loader=None, splitter=None, vectorstore_class=None, embedding_model=None):
+#         self.loader = loader
+#         self.splitter = splitter
+#         self.vectorstore_class = vectorstore_class
+#         self.vectorstore = None
+#         self.embedding_model = embedding_model
+        
+#         default_config = {
+#             "loader": UploadPDFLoader,
+#             "splitter": RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100),
+#             "vectorstore_class": Chroma,
+#             "embedding_model": VertexAIEmbeddings(model='textembedding-gecko')
+#         }
+#         if loader is None:
+#             self.loader = default_config["loader"]
+#         if splitter is None:
+#             self.splitter = default_config["splitter"]
+#         if vectorstore_class is None:
+#             self.vectorstore_class = default_config["vectorstore_class"]
+#         if embedding_model is None:
+#             self.embedding_model = default_config["embedding_model"]
+
+#     def load_PDFs(self, files) -> List[Document]:
+#         total_loaded_files = []
+        
+#         total_loaded_files = self.loader().load(files)
             
-            bytes_io_obj = download_from_gcs(self.bucket_name, file_path)
-            loaded_files.append((bytes_io_obj, file_type))
-            logger.info(f"Successfully loaded file: {file_object.filename}")
-
-        file_loader = self.loader(loaded_files)
-        file_documents = file_loader.load()
-
-        return file_documents
-
+#         return total_loaded_files
+    
+#     def split_loaded_documents(self, loaded_documents: List[Document], verbose = False) -> List[Document]:
+#         total_chunks = []
+#         chunks = self.splitter.split_documents(loaded_documents)
+#         total_chunks.extend(chunks)
+#         if verbose:
+#             logger.info(f"Split {len(total_chunks)} chunks")
+#         return total_chunks
+    
+#     def create_vectorstore(self, documents: List[Document], verbose = False):
+#         self.vectorstore = self.vectorstore_class.from_documents(documents, self.embedding_model)
+#         if verbose:
+#             logger.info(f"Successfully created vectorstore")
+#         return self.vectorstore
+    
+#     def compile(self):
+#         # Compile the pipeline
+#         self.load_PDFs = RAGRunnable(self.load_PDFs)
+#         self.split_loaded_documents = RAGRunnable(self.split_loaded_documents)
+#         self.create_vectorstore = RAGRunnable(self.create_vectorstore)
+#         logger.info(f"Successfully compiled pipeline")
+    
+#     def clear(self):
+#         return self.vectorstore.delete_collection()
+    
+#     def __call__(self, documents):
+#         # Returns a vectorstore ready for usage 
+#         pipeline = self.load_PDFs | self.split_loaded_documents | self.create_vectorstore
+#         return pipeline(documents)
 class RAGpipeline:
     def __init__(self, loader=None, splitter=None, vectorstore_class=None, embedding_model=None):
-        self.loader = loader
-        self.splitter = splitter
-        self.vectorstore_class = vectorstore_class
-        self.vectorstore = None
-        self.embedding_model = embedding_model
-        
         default_config = {
             "loader": UploadPDFLoader,
             "splitter": RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100),
             "vectorstore_class": Chroma,
             "embedding_model": VertexAIEmbeddings(model='textembedding-gecko')
         }
-        if loader is None:
-            self.loader = default_config["loader"]
-        if splitter is None:
-            self.splitter = default_config["splitter"]
-        if vectorstore_class is None:
-            self.vectorstore_class = default_config["vectorstore_class"]
-        if embedding_model is None:
-            self.embedding_model = default_config["embedding_model"]
+        self.loader = loader or default_config["loader"]
+        self.splitter = splitter or default_config["splitter"]
+        self.vectorstore_class = vectorstore_class or default_config["vectorstore_class"]
+        self.embedding_model = embedding_model or default_config["embedding_model"]
 
     def load_PDFs(self, files) -> List[Document]:
-        total_loaded_files = []
-        
-        total_loaded_files = self.loader().load(files)
-            
+        loader_instance = self.loader(files)
+        total_loaded_files = loader_instance.load()
         return total_loaded_files
     
-    def split_loaded_documents(self, loaded_documents: List[Document], verbose = False) -> List[Document]:
+    def split_loaded_documents(self, loaded_documents: List[Document], verbose=False) -> List[Document]:
         total_chunks = []
         chunks = self.splitter.split_documents(loaded_documents)
         total_chunks.extend(chunks)
@@ -148,7 +245,7 @@ class RAGpipeline:
             logger.info(f"Split {len(total_chunks)} chunks")
         return total_chunks
     
-    def create_vectorstore(self, documents: List[Document], verbose = False):
+    def create_vectorstore(self, documents: List[Document], verbose=False):
         self.vectorstore = self.vectorstore_class.from_documents(documents, self.embedding_model)
         if verbose:
             logger.info(f"Successfully created vectorstore")
@@ -168,6 +265,7 @@ class RAGpipeline:
         # Returns a vectorstore ready for usage 
         pipeline = self.load_PDFs | self.split_loaded_documents | self.create_vectorstore
         return pipeline(documents)
+
     
 class QuestionChoice(BaseModel):
     key: str = Field(description="A unique identifier for the choice")
@@ -191,7 +289,8 @@ class QuizBuilder:
         }
         
         if prompt is None:
-            self.prompt = read_blob_to_string(bucket_name="backend-prompt-lib", file_path="quizzify/quizzify-prompt.txt")
+            config = read_text_file("prompt/quizzify-prompt.txt")
+            self.prompt = config['template']
         if vectorstore is None:
             raise ValueError("Vectorstore must be provided")
         if model is None:
@@ -235,5 +334,6 @@ class QuizBuilder:
         self.vectorstore.delete_collection()
         
         return generated_questions
+
     
         
