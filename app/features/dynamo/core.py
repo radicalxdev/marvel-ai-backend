264f71afa5e01db9d4a40b6c538f73deb72fde43
Dylanz <dinglunz@usc.edu>
2024-07-02 12:22:58 -0700

    BytesIO inputs & Integration

--- app/features/dynamo/core.py
+++ app/features/dynamo/core.py
@@ -1,11 +1,18 @@
-from app.features.dynamo.tools import summarize_transcript, generate_flashcards
+from app.features.dynamo.tools import summarize_transcript, generate_flashcards, load_documents, summarize_docs
 from app.services.logger import setup_logger
 from app.api.error_utilities import VideoTranscriptError
+from app.services.tool_registry import ToolFile
 
 logger = setup_logger(__name__)
 
-def executor(youtube_url: str, verbose=False):
-    summary = summarize_transcript(youtube_url, verbose=verbose)
+def executor(youtube_url: str, files: list[ToolFile], verbose=False):
+    # New method
+    documents = load_documents(youtube_url, files)
+    summary = summarize_docs(documents)
+
+    # Previous method
+    # summary = summarize_transcript(youtube_url, verbose=verbose)
+    
     flashcards = generate_flashcards(summary)
 
     sanitized_flashcards = []
