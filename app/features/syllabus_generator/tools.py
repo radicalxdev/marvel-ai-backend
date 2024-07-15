e17efa9b5ff64c1e4c9d0aa7f41b038dc6d41281
DariusChit <darius.chitoroaga@proton.me>
2024-07-15 18:16:12 +0100

    made pydantic object for syllabus_generator

--- app/features/syllabus_generator/tools.py
+++ app/features/syllabus_generator/tools.py
@@ -2,7 +2,9 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_google_genai import GoogleGenerativeAI
+from pydantic import BaseModel, Field
 from app.services.logger import setup_logger
+from typing import List, Dict
 import os
 
 logger = setup_logger(__name__)
@@ -32,7 +34,7 @@ class SyllabusBuilder:
     ):
         default_config = {
             "model": GoogleGenerativeAI(model="gemini-1.0-pro"),
-            "parser": JsonOutputParser(),
+            "parser": JsonOutputParser(pydantic_object=SyllabusModel),
             "prompt": read_text_file("prompt/syllabus_prompt.txt"),
         }
 
@@ -90,3 +92,65 @@ class SyllabusBuilder:
             {"subject": self.subject, "grade_level": self.grade_level}
         )
         return response
+
+
+class SyllabusModel(BaseModel):
+    title: str = Field(description="The title for the whole course")
+    overview: str = Field(
+        description="A broad overview of what is expected of students and what they will learn"
+    )
+    objectives: Dict[int, str] = Field(
+        description="A numbered list of of specific taskts the student will be able to successfully do upon completion of the course",
+        examples=[
+            {
+                1: "Define the key terms associated with electrocardiographs.",
+                2: "Describe the cardiac cycle and the conduction systems that controls the cardiac cycle.",
+                3: "Describe the electrocardiogram.",
+            },
+            {
+                1: "Craft short plays with clear action, developed characters, and precise dialogue",
+                2: "Apply feedback to your own writing through revision",
+                3: "Analyse and discuss the craft of contemporary plays",
+            },
+        ],
+    )
+    policies_and_exceptions: List[str] = Field(
+        description="Class policies, exceptions, important rules and any special consideration all students must be aware of"
+    )
+    # resources: List[str] = Field(
+    #     description="A list of exisiting resources such as books, websites that can be used to learn key topics for the course"
+    # )
+    # This can be expanded
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": """
+            {
+            "title": "ELECTROCARDIOGRAPH TECHNIQUE & APPLICATION",
+            "overview": "Acquiring a deeper understanding of the cardiovascular system and how it functions, students
+                practice basic electrocardiograph patient care techniques, applying legal and ethical responsibilities. Students learn the
+                use of medical instrumentation, electrocardiogram theory, identification of and response to mechanical problems,
+                recognition of cardiac rhythm and response to emergency findings.",
+            "objectives":
+                {
+                1: "Define the key terms associated with electrocardiographs.",
+                2: "Describe the cardiac cycle and the conduction systems that controls the cardiac cycle.",
+                3: "Describe the electrocardiogram.",
+                4: "Maintain equipment for safety and accuracy; identify and eliminate or report interference and
+                    mechanical problems."
+                },
+                policies_and_exceptions: [
+                    "ATTENDANCE REQUIREMENTS:
+                    It is important for the school to be notified when a student is not able to attend class. It is the student’s responsibility to
+                    inquire about make-up work for both classroom lectures and laboratory sessions.
+                    Tardiness and/or absence from any part of a class/lab will constitute a partial absence. A total of three partial absences
+                    will constitute a full absence. ",
+                    "MAKE-UP WORK:
+                    It is the student’s responsibility to inquire about make-up work for both classroom and laboratory sessions. The
+                    instructor will not re-teach material, therefore there is no charge for make-up work. For information regarding make-up
+                    work."
+                ]
+            }
+            """
+        }
+    }
