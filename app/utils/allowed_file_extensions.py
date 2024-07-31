d6a593a6d20a12371bbc18fa909686bd940b181f
WilfredoAaronSosaRamosBoushtech <wilfredo.sosa@boushtech.net>
2024-07-31 17:06:34 -0500

    new epic 7.1 imp

---
+++ app/utils/allowed_file_extensions.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class FileType(Enum):
+    PDF = 'pdf'
+    CSV = 'csv'
+    TXT = 'txt'
+    MD = 'md'
+    URL = "url"
+    PPTX = 'pptx'
+    DOCX = "docx"
+    XLS = "xls"
+    XLSX = "xlsx"
+    XML = 'xml'
+
+    GDOC = 'gdoc'
+    GSHEET = "gsheet"
+    GSLIDE = "gslide"
+    GPDF = 'gpdf'
+
+    YOUTUBE_URL = 'youtube_url' 
+    IMG = 'img'
