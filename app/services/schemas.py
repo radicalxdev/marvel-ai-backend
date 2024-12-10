080ceb4ed83263b76ff00407e9b0fcffe83e9fd1
asleem <abir.sleem06@gmail.com>
2024-12-10 14:28:30 -0500

    fix PR conflicts

--- app/services/schemas.py
+++ app/services/schemas.py
@@ -3,7 +3,6 @@ from typing import Optional, List, Any, Literal
 from enum import Enum
 from app.services.tool_registry import BaseTool
 
-
 class User(BaseModel):
     id: str
     fullName: str
@@ -53,6 +52,78 @@ class ChatMessage(BaseModel):
     role: str
     type: str
     text: str
+
+class QuizzifyArgs(BaseModel):
+    topic: str
+    n_questions: int
+    file_url: str
+    file_type: str
+    lang: Optional[str] = "en"
+
+class WorksheetQuestion(BaseModel):
+    question_type: str
+    number: int
+    
+class WorksheetQuestionModel(BaseModel):
+    worksheet_question_list: List[WorksheetQuestion]
+
+class WorksheetGeneratorArgs(BaseModel):
+    grade_level: str
+    topic: str
+    worksheet_list: WorksheetQuestionModel
+    file_url: str
+    file_type: str
+    lang: Optional[str] = "en"
+    
+class SyllabusGeneratorArgsModel(BaseModel):
+    grade_level: str
+    subject: str
+    course_description: str
+    objectives: str
+    required_materials: str
+    grading_policy: str
+    policies_expectations: str
+    course_outline: str
+    additional_notes: str
+    file_url: str
+    file_type: str
+    lang: Optional[str] = "en"
+    
+class AIResistantArgs(BaseModel):
+    assignment: str = Field(..., min_length=1, max_length=255, description="The given assignment")
+    grade_level: Literal["pre-k", "kindergarten", "elementary", "middle", "high", "university", "professional"] = Field(..., description="Educational level to which the content is directed")
+    file_type: str = Field(..., description="Type of file being handled, according to the defined enumeration")
+    file_url: str = Field(..., description="URL or path of the file to be processed")
+    lang: str = Field(..., description="Language in which the file or content is written")
+    
+class ConnectWithThemArgs(BaseModel):
+    grade_level: str = Field(..., description="The grade level the teacher is instructing.")
+    task_description: str = Field(..., description="A brief description of the subject or topic the teacher is instructing.")
+    students_description: str = Field(..., description="A description of the students including age group, interests, location, and any relevant cultural or social factors.")
+    file_url: str = Field(..., description="URL of any relevant file associated with the teaching material.")
+    file_type: str = Field(..., description="The type of the file")
+    lang: str = Field(..., description="The language in which the subject is being taught.")
+
+class PresentationGeneratorInput(BaseModel):
+    grade_level: str
+    n_slides: int
+    topic: str
+    objectives: str
+    additional_comments: str
+    file_url: str
+    file_type: str
+    lang: Optional[str] = "en"
+
+class RubricGeneratorArgs(BaseModel):
+    grade_level: Literal["pre-k", "kindergarten", "elementary", "middle", "high", "university", "professional"]
+    point_scale: int
+    objectives: str
+    assignment_desc: str
+    objectives_file_url: str
+    objectives_file_type: str
+    ad_file_url: str
+    ad_file_type: str
+    lang: Optional[str]
 
 class NotesGeneratorArgs(BaseModel):
     topic: str = Field(..., description="Topic of the notes")
