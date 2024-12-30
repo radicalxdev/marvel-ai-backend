e718e354e465c79e478be9fb737a4f11d104da0d
WilfredoAaronSosaRamosBoushtech <113047749+AaronSosaRamos@users.noreply.github.com>
2024-12-30 17:39:39 -0500

    advanced coteacher imp.

--- app/services/schemas.py
+++ app/services/schemas.py
@@ -104,10 +104,10 @@ class ConnectWithThemArgs(BaseModel):
     grade_level: str = Field(..., description="The grade level the teacher is instructing.")
     task_description: str = Field(..., description="A brief description of the subject or topic the teacher is instructing.")
     students_description: str = Field(..., description="A description of the students including age group, interests, location, and any relevant cultural or social factors.")
-    td_file_url: str 
-    td_file_type: str 
-    sd_file_url: str
-    sd_file_type: str
+    task_description_file_url: str 
+    task_description_file_type: str 
+    student_description_file_url: str
+    student_description_file_type: str
     lang: str = Field(..., description="The language in which the subject is being taught.")
 
 class PresentationGeneratorInput(BaseModel):
@@ -118,19 +118,19 @@ class PresentationGeneratorInput(BaseModel):
     additional_comments: str
     objectives_file_url: str
     objectives_file_type: str
-    ac_file_url: str
-    ac_file_type: str
+    additional_comments_file_url: str
+    additional_comments_file_type: str
     lang: Optional[str] = "en"
 
 class RubricGeneratorArgs(BaseModel):
     grade_level: Literal["pre-k", "kindergarten", "elementary", "middle", "high", "university", "professional"]
     point_scale: int
     objectives: str
-    assignment_desc: str
+    assignment_description: str
     objectives_file_url: str
     objectives_file_type: str
-    ad_file_url: str
-    ad_file_type: str
+    assignment_description_file_url: str
+    assignment_description_file_type: str
     lang: Optional[str]
 
 class LessonPlanGeneratorArgs(BaseModel):
@@ -140,8 +140,8 @@ class LessonPlanGeneratorArgs(BaseModel):
     additional_customization: str
     objectives_file_url: str
     objectives_file_type: str
-    ac_file_url: str
-    ac_file_type: str
+    additional_customization_file_url: str
+    additional_customization_file_type: str
     lang: Optional[str] = "en"
 
 class WritingFeedbackGeneratorArgs(BaseModel):
@@ -151,6 +151,6 @@ class WritingFeedbackGeneratorArgs(BaseModel):
     writing_to_review: str
     criteria_file_url: str
     criteria_file_type: str
-    wtr_file_url: str
-    wtr_file_type: str
+    writing_to_review_file_url: str
+    writing_to_review_file_type: str
     lang: Optional[str] = "en"
--- app/utils/actions_for_assistants/actions_for_assistants.py
+++ app/utils/actions_for_assistants/actions_for_assistants.py
@@ -5,12 +5,22 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv, find_dotenv
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langgraph.graph import END
 from pydantic import BaseModel, Field
+from langchain_core.runnables.base import RunnableLambda
+from app.services.logger import setup_logger
+from app.tools.utils.tool_utilities import (
+    load_tool_metadata, 
+    execute_tool, 
+    finalize_inputs
+)
+
+from app.services.tool_registry import BaseTool
 
 load_dotenv(find_dotenv())
 
-chat_google_genai = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
+logger = setup_logger()
+
+chat_google_genai = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp", temperature=0)
 
 def read_text_file(file_path):
     # Get the directory containing the script file
@@ -21,117 +31,251 @@ def read_text_file(file_path):
     
     with open(absolute_file_path, 'r') as file:
         return file.read()
+    
 
-def process_content(
-    state
-):
-    """
-    This function, `process_content`, is designed to dynamically generate a response based on the user's action and context. 
-    It takes in a `state` dictionary that contains information such as the user's query, the intended action (e.g., translate, 
-    summarize, rewrite, generate questions, or a custom task), and chat history for added context.
-
-    The function performs the following steps:
-    1. Constructs the chat history string to provide context for processing.
-    2. Maps specific actions to corresponding prompt templates and logic using `action_map`.
-    3. Invokes a generative AI model (`chat_google_genai`) with the appropriate system and user messages based on the action selected.
-    4. Returns the processed result as a response.
-
-    Each action retrieves a predefined template from corresponding prompt files and appends the user query and chat history for 
-    contextualized processing. If an invalid action is passed, the function raises an error.
-    """
-
-    chat_history = " For context, you can use the chat history provided: " + " ".join(
-        f"[{message.role} - {message.type}]: {message.text}" for message in state["chat_history"]
-    )
-
-    action_map = {
-        "translate": lambda: [
-            ("system", state["assistant_system_message"]+read_text_file('prompts/translate.txt')), 
-            ("human", state["user_query"]+chat_history)
-        ],
-        "summarize": lambda: [
-            ("system", state["assistant_system_message"]+read_text_file('prompts/summarize.txt')),  
-            ("human", state["user_query"]+chat_history)
-        ],
-        "rewrite": lambda: [
-            ("system", state["assistant_system_message"]+read_text_file('prompts/rewrite.txt')),  
-            ("human", state["user_query"]+chat_history)
-        ],
-        "question_generation": lambda: [
-            ("system", state["assistant_system_message"]+read_text_file('prompts/question_generation.txt')),  
-            ("human", state["user_query"]+chat_history)
-        ],
-        "custom": lambda: [
-            ("system", state["assistant_system_message"]+read_text_file('prompts/custom_prompt.txt')),  
-            ("human", state["user_query"]+chat_history)
-        ],
-        "default": lambda: [
-            ("system", state["assistant_system_message"]),  
-            ("human", state["user_query"]+chat_history)
-        ],
-    }
-
-    if state["action"] not in action_map:
-        raise ValueError(f"Action '{state['action']}' is not a valid action in the action_map")
-
-    messages = action_map[state["action"]]()
-    response = chat_google_genai.invoke(messages)
+def adapt_questions_for_json(output, parser):
     return {
-        "result": response.content
+        "questions": output,
+        "format_instructions": parser.get_format_instructions()
     }
 
-def generate_questions_to_json(state):
-    """
-    The function `generate_questions_to_json` is designed to transform a list of questions into a structured JSON format. 
-    It utilizes a predefined parsing schema and a generative AI chain to process the input and produce the desired output.
+marvel_ai_tools_prefix = "prompts/marvel_ai_tools/"
 
-    Key steps in the function:
-    1. Parser Setup: Initializes a `JsonOutputParser` using the `QuestionList` schema to define the expected JSON structure.
-    2. Prompt Definition: Loads system and user prompt templates from external files (`prompts/system_prompt_question_generation_json.txt` 
-    and `prompts/user_prompt_question_generation_json.txt`) to provide instructions and context for generating the JSON.
-    3. AI Chain Setup: Combines the prompt and generative AI model (`chat_google_genai`) into a processing chain.
-    4. Invocation: Calls the chain with the questions (from `state["result"]`) and format instructions provided by the parser, ensuring the 
-    output adheres to the predefined JSON schema.
-    5. Result Parsing: Parses the AI-generated content using the parser to ensure it matches the structured format.
+def adapt_marvel_tool_id(output, user_query, parser):
+    return {
+        "tool_id": output.strip(),
+        "user_query": user_query,
+        "request_sample": marvel_ai_tools_prefix+marvel_ai_tools_prompts[output.strip()],
+        "format_instructions": parser.get_format_instructions()
+    }
 
-    The function returns a dictionary containing the `result`, which is the parsed JSON object representing the list of questions in 
-    a structured format.
-    """
-    parser = JsonOutputParser(pydantic_object=QuestionList)
+def translation_tool(user_query: str, source_lang: str, target_lang: str):
+    """ Used for translating content from a source language to a target language. """
     prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            read_text_file('prompts/system_prompt_question_generation_json.txt')
-        ),
-        ("human", read_text_file('prompts/user_prompt_question_generation_json.txt'))
-    ]
+        [
+            (
+                "system",
+                read_text_file("prompts/system/translation_tool.txt")
+            ),
+            (
+                "human",
+                read_text_file("prompts/human/translation_tool.txt")
+            )
+        ]
     )
 
     chain = prompt | chat_google_genai
+    result = chain.invoke({
+        "user_query": user_query,
+        "source_lang": source_lang,
+        "target_lang": target_lang
+    })
+    return result.content
 
-    result = chain.invoke(
-        {
-            "questions": state["result"],
-            "format_instructions": parser.get_format_instructions()
-        }
+def summarization_tool(user_query: str, summarization_type: str):
+    """ Used for summarize content"""
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                read_text_file("prompts/system/summarization_tool.txt")
+            ),
+            (
+                "human",
+                read_text_file("prompts/human/summarization_tool.txt")
+            )
+        ]
     )
-    
-    return {
-        "result": parser.parse(result.content)
-    }
-    
 
-def go_to_process_questions_json(state):
-    """
-    Determines the next processing step based on the action in the `state`. 
-    Returns `'generate_questions_to_json'` if the action is `'question_generation'`, otherwise returns `END`.
-    """
+    chain = prompt | chat_google_genai
+    result = chain.invoke({
+        "user_query": user_query,
+        "summarization_type": summarization_type
+    })
+    return result.content
 
-    if state['action'] == 'question_generation':
-        return 'generate_questions_to_json'
-    else:
-        return END
+def rewrite_tool(user_query: str, focus: str, tone: str):
+    """ Used for rewriting content"""
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                read_text_file("prompts/system/rewrite_tool.txt")
+            ),
+            (
+                "human",
+                read_text_file("prompts/human/rewrite_tool.txt")
+            )
+        ]
+    )
+
+    chain = prompt | chat_google_genai
+    result = chain.invoke({
+        "user_query": user_query,
+        "focus": focus,
+        "tone": tone
+    })
+    return result.content
+
+def custom_prompt_tool(user_query: str, action: str):
+    """ Used for managing custom prompts"""
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                read_text_file("prompts/system/custom_prompt_tool.txt")
+            ),
+            (
+                "human",
+                read_text_file("prompts/human/custom_prompt_tool.txt")
+            )
+        ]
+    )
+
+    chain = prompt | chat_google_genai
+    result = chain.invoke({
+        "user_query": user_query,
+        "action": action
+    })
+    return result.content
+
+
+def generate_questions_to_json_tool(user_query: str):
+    """ Used for generating questions. The results are returned in a JSON format. You must respect the user's language """
+    prompt_generate_questions = ChatPromptTemplate.from_messages(
+      [
+          (
+            "system",
+            read_text_file("prompts/system/question_generation.txt")
+          ),
+          ("human",
+            read_text_file("prompts/human/question_generation.txt")
+          )
+      ]
+    )
+
+    chain_generate_questions = prompt_generate_questions | chat_google_genai
+
+    parser = JsonOutputParser(pydantic_object=QuestionList)
+    prompt_questions_to_json = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            read_text_file("prompts/system/generate_questions_to_json_tool.txt")
+        ),
+        ("human",
+            read_text_file("prompts/human/generate_questions_to_json_tool.txt")
+        )
+    ]
+    )
+
+    chain_questions_to_json = prompt_questions_to_json | chat_google_genai
+    combined_chain = (
+        chain_generate_questions
+        | RunnableLambda(lambda x: adapt_questions_for_json(x.content, parser))
+        | chain_questions_to_json
+        | parser
+    )
+
+    result = combined_chain.invoke(user_query)
+
+    return result
+
+def call_marvel_ai_tool(user_query: str):
+    """Used for calling any of the following Marvel AI tools:
+    1. Multiple Choice Quiz Generator
+    2. Flashcards Generator
+    3. Worksheet Generator
+    4. Syllabus Generator
+    5. AI-Resistant Assignments
+    6. Connect with Them
+    7. Presentation Generator
+    8. Rubric Generator
+    9. Lesson Plan Generator
+    10. Writing Feedback Generator
+    """
+    prompt_identifier = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                read_text_file("prompts/system/generate_marvel_ai_tool_identifier.txt")
+            ),
+            (
+                "human",
+                read_text_file("prompts/human/generate_marvel_ai_tool_identifier.txt")
+            )
+        ]
+    )
+
+    chain_identifier = prompt_identifier | chat_google_genai
+    
+    prompt_generate_metadata = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                read_text_file("prompts/system/generate_marvel_ai_tool_metadata.txt")
+            ),
+            (
+                "human",
+                read_text_file("prompts/human/generate_marvel_ai_tool_metadata.txt")
+            )
+        ]
+    )
+
+    chain_generate_metadata = prompt_generate_metadata | chat_google_genai
+
+    parser = JsonOutputParser(pydantic_object=ToolRequest)
+
+    combined_chain = (
+        chain_identifier
+        | RunnableLambda(lambda x: adapt_marvel_tool_id(x.content, user_query, parser))
+        | chain_generate_metadata
+        | parser
+    )
+
+    result = combined_chain.invoke(user_query)
+
+    logger.info(f"Metadata generated: {result}")
+
+    result_with_schema = ToolRequest.model_validate(result)
+
+    request_data = result_with_schema.tool_data    
+    requested_tool = load_tool_metadata(request_data.tool_id)
+    request_inputs_dict = finalize_inputs(request_data.inputs, requested_tool["inputs"])
+    final_result = execute_tool(request_data.tool_id, request_inputs_dict)
+
+    return get_dict_or_value(final_result)
+
+def get_dict_or_value(value):
+    """
+    If the input value is an instance of BaseModel, return its dictionary representation.
+    Otherwise, return the value itself.
+    """
+    if isinstance(value, BaseModel):
+        return value.model_dump()
+    return value
+
+def default_prompt_tool(user_query: str):
+    """Used for answering any kind of prompt that is not about Translation, Summarization,
+    Rewriting, Question Generation, or Custom Prompts"""
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                read_text_file("prompts/system/default_prompt_tool.txt")
+            ),
+            (
+                "human",
+                read_text_file("prompts/human/default_prompt_tool.txt")
+            )
+        ]
+    )
+
+    chain = prompt | chat_google_genai
+    result = chain.invoke({
+        "user_query": user_query
+    })
+    return result.content
 
 class QuestionChoice(BaseModel):
     key: str = Field(description="A unique identifier for the choice using letters A, B, C, or D.")
@@ -150,3 +294,39 @@ class OpenEndedQuestion(BaseModel):
 class QuestionList(BaseModel):
     multiple_choice_questions: List[MultipleChoiceQuestion]
     open_ended_questions: List[OpenEndedQuestion]
+
+class ToolRequest(BaseModel):
+    tool_data: BaseTool
+
+actions = [
+    translation_tool,
+    summarization_tool,
+    rewrite_tool,
+    generate_questions_to_json_tool,
+    custom_prompt_tool,
+    default_prompt_tool,
+    call_marvel_ai_tool
+]
+
+function_map = {
+    "translation_tool": translation_tool,
+    "summarization_tool": summarization_tool,
+    "rewrite_tool": rewrite_tool,
+    "generate_questions_to_json_tool": generate_questions_to_json_tool,
+    "custom_prompt_tool": custom_prompt_tool,
+    "default_prompt_tool": default_prompt_tool,
+    "call_marvel_ai_tool": call_marvel_ai_tool
+}
+
+marvel_ai_tools_prompts = {
+    "ai-resistant-assignments-generator": "ai_resistant_assignments_generator_request_sample.txt",
+    "connect-with-them": "connect_with_them_request_sample.txt",
+    "flashcard-generator": "flashcards_generator_request_sample.txt",
+    "lesson-generator": "lesson_plan_generator_request_sample.txt",
+    "multiple-choice-quiz-generator": "multiple_choice_quiz_generator_request_sample.txt",
+    "presentation-generator": "presentation_generator_request_sample.txt",
+    "rubric-generator": "rubric_generator_request_sample.txt",
+    "syllabus-generator": "syllabus_generator_request_sample.txt",
+    "worksheet-generator": "worksheet_generator_request_sample.txt",
+    "writing-feedback-generator": "writing_feedback_generator_request_sample.txt",
+}
